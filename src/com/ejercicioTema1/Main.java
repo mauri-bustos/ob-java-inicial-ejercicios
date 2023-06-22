9b9546e6438c6e0f35ac01adea065d6010079486
Mauricio Bustos <mauricio.n.bustos@gmail.com>
2023-06-22 13:50:07 -0300

    Entrega ejercicio tema2

--- src/com/ejercicioTema1/Main.java
+++ src/com/ejercicioTema1/Main.java
@@ -1,40 +1,40 @@
 package com.ejercicioTema1;
 
 public class Main {
-	
+
 	public static void main(String[] args) {
-		
-		//datos de tipo enteros
+
+		// datos de tipo enteros
 		byte numero1 = 45;
 		short numero2 = 2588;
 		int numero3 = 344471000;
 		long numero4 = 20003455678L;
-		
+
 		System.out.println(numero1);
 		System.out.println(numero2);
 		System.out.println(numero3);
 		System.out.println(numero4);
-		
-		//tipo de datos con coma flotante
+
+		// tipo de datos con coma flotante
 		double numero5 = 20.4;
 		float numero6 = 3.54f;
-		
+
 		System.out.println(numero5);
 		System.out.println(numero6);
-		
-		//caracter
+
+		// caracter
 		char caracter1 = 'M';
-		
+
 		System.out.println(caracter1);
-		
-		//cadena de texto
+
+		// cadena de texto
 		String nombre = "Mauricio";
-		
+
 		System.out.println(nombre);
-		
-		//boolean -> Verdaderdo o Falso
+
+		// boolean -> Verdaderdo o Falso
 		boolean verificar = true;
-		
+
 		System.out.println(verificar);
 	}
 
---
+++ src/com/ejercicioTema2/Main.java
@@ -0,0 +1,22 @@
+package com.ejercicioTema2;
+
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Ingrese el precio");
+		double precio = scanner.nextDouble();
+		double precioFinal = calcularIva(precio);
+		System.out.println(precioFinal);
+		
+	}
+	
+	static double calcularIva(double precio) {
+		return precio + (precio * 0.21); // IVA de Argentina
+	}
+
+}
