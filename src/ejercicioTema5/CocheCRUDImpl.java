84d38f2d8615673a0a5f93155bdd6c827a228d3c
Mauricio Bustos <mauricio.n.bustos@gmail.com>
2023-06-24 07:01:35 -0300

    Entrega ejercicioTema5

---
+++ src/ejercicioTema5/CocheCRUDImpl.java
@@ -0,0 +1,21 @@
+package ejercicioTema5;
+
+public class CocheCRUDImpl implements CocheCRUD{
+
+	@Override
+	public void save() {
+		System.out.println("Método save()");
+	}
+
+	@Override
+	public void findAll() {
+		System.out.println("Método findAll()");
+	}
+
+	@Override
+	public void delete() {
+		System.out.println("Método delete()");
+		
+	}
+
+}
